119dcb9c74fe0df21d7a1585b2a732069e97c852
Ioannes Brastianos <john.bracciano@gmail.com>
2023-01-20 19:42:41 +0100

    Add num type tests

---
+++ src/types/spec/num.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from 'vitest'
+import { num, typeOf } from '..'
+
+describe('[int]', () => {
+
+    test('has correct type name', () => {
+        expect(typeOf(num)).toBe('num')
+    })
+
+    test('returns the numeric value it was called with back to the caller', () => {
+        const numbers = Array.from({ length: 100 }, () => (
+            (Math.random() * Number.MAX_SAFE_INTEGER) * (
+                Math.random() < 0.5 ? -1 : 1
+            )
+        ))
+
+        numbers.forEach(value => {
+            expect(num(value)).toBe(value)
+        })
+    })
+
+    test('throws when called with a value that is not a number', () => {
+        expect(() => num('')).toThrow()
+        expect(() => num('0')).toThrow()
+        expect(() => num(['1'])).toThrow()
+        expect(() => num(true)).toThrow()
+        expect(() => num({})).toThrow()
+        expect(() => num(null)).toThrow()
+        expect(() => num(undefined)).toThrow()
+        expect(() => num(() => 1)).toThrow()
+    })
+})
