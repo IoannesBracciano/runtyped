428b785f2a128aa49ad8a2d0e0fe01b25e214c13
Ioannes Brastianos <john.bracciano@gmail.com>
2023-01-20 20:18:04 +0100

    Add tests for `opt` type

---
+++ src/types/spec/opt.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from 'vitest'
+import { opt, str, typeOf } from '..'
+
+describe('[opt<str>]', () => {
+
+    const optStr = opt(str)
+
+    test('has correct type name', () => {
+        expect(typeOf(optStr)).toBe('opt<str>')
+    })
+
+    test('returns the string value it was called with back to the caller', () => {
+        expect(optStr('')).toBe('')
+        expect(optStr(String(''))).toBe('')
+        expect(optStr('string')).toBe('string')
+    })
+
+    test('returns `undefined` back to the caller', () => {
+        expect(optStr(undefined)).toBe(undefined)
+    })
+
+    test('throws when called with a value that is not a string, `undefined`', () => {
+        expect(() => optStr(0)).toThrow()
+        expect(() => optStr(1)).toThrow()
+        expect(() => optStr(-1)).toThrow()
+        expect(() => optStr(['1'])).toThrow()
+        expect(() => optStr(true)).toThrow()
+        expect(() => optStr({})).toThrow()
+        expect(() => optStr(null)).toThrow()
+        expect(() => optStr(() => 1)).toThrow()
+    })
+})
