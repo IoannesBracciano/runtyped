b20a34fe0c0e5f04c2fc2c37b192e8c032633d7b
Ioannes Brastianos <john.bracciano@gmail.com>
2023-01-20 20:10:11 +0100

    Add tests for `str` type

---
+++ src/types/spec/str.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest'
+import { str, typeOf } from '..'
+
+describe('[str]', () => {
+
+    test('has correct type name', () => {
+        expect(typeOf(str)).toBe('str')
+    })
+
+    test('returns the string value it was called with back to the caller', () => {
+        expect(str('')).toBe('')
+        expect(str(String(''))).toBe('')
+        expect(str('string')).toBe('string')
+    })
+
+    test('throws when called with a value that is not a string', () => {
+        expect(() => str(0)).toThrow()
+        expect(() => str(1)).toThrow()
+        expect(() => str(-1)).toThrow()
+        expect(() => str(['1'])).toThrow()
+        expect(() => str(true)).toThrow()
+        expect(() => str({})).toThrow()
+        expect(() => str(null)).toThrow()
+        expect(() => str(undefined)).toThrow()
+        expect(() => str(() => 1)).toThrow()
+    })
+})
