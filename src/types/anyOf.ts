0e5b29d9d0f396dab493c1963b6f1cee8bced863
Ioannes Brastianos <john.bracciano@gmail.com>
2023-01-21 15:13:00 +0100

    Automatically wrap naked values with the `id` type when passed to `anyOf`

--- src/types/anyOf.ts
+++ src/types/anyOf.ts
@@ -1,7 +1,14 @@
 import { createType, Type, typename } from '../runtyped'
+import { id } from './id'
 
-export const anyOf = <T extends readonly Type[]>(...types: T) =>
-    createType<T[number] extends Type<infer S> ? S : never>(
-        `anyOf<${types.map(typename).join(',')}>`, 
-        value => types.some(({ assert }) => assert(value)),
+export const anyOf = <T extends readonly any[]>(...types: T) => {
+    const tnormalized = types.map(type => (
+        typeof type !== 'function' || !typename(type)
+            ? id(type)
+            : type
+    ))
+    return createType<T[number] extends Type<infer S> ? S : T[number]>(
+        `anyOf<${tnormalized.map(typename).join(',')}>`, 
+        value => tnormalized.some(({ assert }) => assert(value)),
     )
+}
--- src/types/spec/anyOf.spec.ts
+++ src/types/spec/anyOf.spec.ts
@@ -2,15 +2,17 @@ import { expect, test } from 'vitest'
 import { anyOf, arrOf, uint } from '..'
 
 test('[anyOf] returns value if it is of any of the types specified', () => {
-    const union = anyOf(uint, arrOf(uint))
-    expect(union(1)).toEqual(1)
+    const union = anyOf(uint, arrOf(uint), 'static' as const)
+    expect(union(1)).toBe(1)
     expect(union([0,1])).toEqual([0,1])
+    expect(union('static')).toBe('static')
 })
 
 test('[anyOf] type throws if value type is none of the types specified', () => {
-    const union = anyOf(uint, arrOf(uint))
+    const union = anyOf(uint, arrOf(uint), 'static' as const)
     expect(() => union(-1)).toThrow()
     expect(() => union('any')).toThrow()
     expect(() => union([1, 2, 'a'])).toThrow()
     expect(() => union(['1'])).toThrow()
+    expect(() => union('st4tic')).toThrow()
 })
